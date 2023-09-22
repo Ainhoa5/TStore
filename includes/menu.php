2efe0e0c42cc441cfb5f41770a94f95c8e8d284f
Ainhoa5 <73906190+Ainhoa5@users.noreply.github.com>
2023-09-22 20:21:29 +0100

    initial commit

---
+++ includes/menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    
+    <ul>
+        <li><a href="act/a01.php">Actividad 1</a></li>
+        <li><a href="act/a02.php">Actividad 2</a></li>
+        <li><a href="act/a03.php">Actividad 3</a></li>
+        <li><a href="act/a04.php">Actividad 4</a></li>
+        <li><a href="act/a05.php">Actividad 5</a></li>
+        <li><a href="act/a06.php">Actividad 6</a></li>
+        <li><a href="act/a07.php">Actividad 7</a></li>
+        <li><a href="act/a08.php">Actividad 8</a></li>
+        <li><a href="act/a09.php">Actividad 9</a></li>
+        <li><a href="act/a010.php">Actividad 10</a></li>
+        <li><a href="act/a011.php">Actividad 11</a></li>
+        <li><a href="act/a012.php">Actividad 12</a></li>
+    </ul>
+</body>
+
+</html>
