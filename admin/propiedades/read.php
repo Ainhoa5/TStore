506e7fd66af6da39bb2031890e0cd000ba17a571
Ainhoa5 <73906190+Ainhoa5@users.noreply.github.com>
2023-10-24 16:41:01 +0100

    read added to index

--- admin/propiedades/read.php
+++ admin/propiedades/read.php
@@ -4,15 +4,15 @@ $path = '../../';
 include $path . 'includes/config/database.php';
 
 // Execute the query
-$result = customQuery("SELECT * FROM Productos WHERE ProductoID = ?", [1], 'i');
+$result = customQuery("SELECT * FROM Productos ", [], '');
 
 // Fetch data and print it out
-if ($result->num_rows > 0) {
+/* if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "ProductoID: " . $row["ProductoID"] . " - Nombre: " . $row["Nombre"] . " - Precio: " . $row["Precio"] . "<br>";
     }
 } else {
     echo "0 results";
-}
+} */
 
 ?>
