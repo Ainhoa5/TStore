f6435b1e4c363ba06e0dcb52e4a91649683f1a80
Ainhoa5 <73906190+Ainhoa5@users.noreply.github.com>
2023-10-24 16:41:15 +0100

    WIP create form

---
+++ admin/propiedades/create.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="create.php">
+        <label for="Nombre">Nombre</label>
+        <input type="text" name="Nombre">
+
+        <label for="Descripción">Descripción</label>
+        <input type="text" name="Descripción">
+
+        <label for="Descripción">Descripción</label>
+        <input type="text" name="Descripción">
+
+        <label for="Precio">Precio</label>
+        <input type="text" name="Precio">
+
+        <label for="Stock">Stock</label>
+        <input type="text" name="Stock">
+
+        <label for="Categoria">Categoria</label>
+        <select name="Categoria" id="Categoria">
+            <option value="Red"></option>
+        </select>
+    </form>
+</body>
+
+</html>
