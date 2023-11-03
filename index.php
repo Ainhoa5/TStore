9b24b11b182dd5e27617e4d8f367edf48e14e79b
Ainhoa5 <ainhoamarianoacosta@gmail.com>
2023-11-03 12:06:35 +0000

    Refactor admin app folder structure and display products in admin view
    - Reorganized the admin application's directory by introducing dedicated folders for MVC components.
    - Created respective models, controllers, and views files within the new directories.
    - Implemented product listing functionality in the admin view.

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
     incluirTemplate('header', true, '');
 ?>
 <!-- Fetch merch -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Index page</h1>
+    <a href="admin/index.php">Admin Panel</a>
+
+</body>
+</html>
