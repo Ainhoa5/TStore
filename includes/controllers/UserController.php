6c969038cd494aea767390b4d733c04a7f03832e
Ainhoa5 <73906190+Ainhoa5@users.noreply.github.com>
2023-12-12 18:39:15 +0000

    WIP User controller and model

--- includes/controllers/UserController.php
+++ includes/controllers/UserController.php
@@ -10,42 +10,44 @@ class ProductController
         $this->model = $model;
     }
 
-    public function showProducts()
+    public function login($email, $password)
     {
-        return $this->model->getProducts();
-    }
-    
-    public function login($email, $password) {
         // Verificar las credenciales y obtener el rol
         $rol = $this->model->verificarCredenciales($email, $password);
-    
+
         // Comprobar si se obtuvo un rol
         if ($rol) {
             // Iniciar la sesión si aún no se ha iniciado
             if (session_status() == PHP_SESSION_NONE) {
                 session_start();
             }
-    
+
             // Guardar datos en la sesión
             $_SESSION['user_email'] = $email;
             $_SESSION['user_role'] = $rol;
-    
+
             // Opcionalmente, guardar el ID del usuario o cualquier otro dato necesario
             // $_SESSION['user_id'] = ...
-    
+
             // Devolver true para indicar un inicio de sesión exitoso
             return true;
         }
-    
+
         // Devolver false si las credenciales no son correctas
         return false;
     }
-    
-    
-    public function checkAdmin($email, $password) 
+
+
+    public function signup($email, $password)
     {
+        // Hashear la contraseña
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+        // Llamar al modelo para guardar los datos del usuario, incluyendo la contraseña hasheada
+        return $this->model->createUser($email, $passwordHash);
+
     }
-    public function logout($email, $password) 
+    public function logout($email, $password)
     {
         // Cerrar sesión.
         session_destroy();
