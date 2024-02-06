6065a0847477fa6c9abbe7a52d260100b189693f
Damián <thedami15@gmail.com>
2024-02-06 19:39:20 +0000

    cart

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ainhoa/t-store',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '85c84583bd8eaaaf1accd67b1ab0f8b91d487d43',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ainhoa/t-store' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '85c84583bd8eaaaf1accd67b1ab0f8b91d487d43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
